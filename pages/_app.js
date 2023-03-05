7669107a2bf885643e5c3ef26c5017be3014c320
mbaytas <m@baytas.net>
2023-03-05 13:52:01 +0100

    add next seo

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+import { DefaultSeo } from "next-seo";
+
 import "../styles/globals.css";
 
 import Layout from "../components/layout";
@@ -5,6 +7,32 @@ import Layout from "../components/layout";
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <DefaultSeo
+        title={undefined}
+        titleTemplate="%s | WEATHERLIGHT"
+        defaultTitle="WEATHERLIGHT"
+        description="venture studio | no signal without noise"
+        openGraph={{
+          type: "website",
+          locale: "en",
+          url: "https://www.weatherlight.com",
+          siteName: "WEATHERLIGHT",
+          images: [
+            {
+              url: "https://www.weatherlight.com/card_default.png",
+              width: 1920,
+              height: 1080,
+              alt: "WEATHERLIGHT",
+              type: "image/png",
+            },
+          ],
+          siteName: "WEATHERLIGHT",
+        }}
+        twitter={{
+          cardType: "summary_large_image",
+        }}
+      />
+
       <Component {...pageProps} />
     </Layout>
   );
